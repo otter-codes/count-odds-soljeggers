60664f953542bc517f22f198717573083b717356
Michael Bailey <michael.bailey3@digital.hmrc.gov.uk>
2018-04-16 11:41:11 +0100

    - Added Unit Tests
    - Added 4 Unit Tests
    - Added code solution in def countOdds
    - All tests Passed

--- src/main/scala/Count.scala
+++ src/main/scala/Count.scala
@@ -1,5 +1,7 @@
 object Count extends App {
 
-  def countOdds (number: Int) = 1
+  def countOdds (number: Int) = {
+    (0 until number).count(number => number % 2 != 0)
+  }
 
 }
--- src/test/scala/CountSpec.scala
+++ src/test/scala/CountSpec.scala
@@ -5,8 +5,18 @@ class CountSpec extends WordSpec with MustMatchers {
   "Count" must {
 
     "When provided with an Int return an Int" in {
-      Count.countOdds(1) mustEqual 1
+      Count.countOdds(0) mustEqual 0
     }
+    "when provided with the number 2 return 1" in {
+      Count.countOdds(2) mustEqual 1
+    }
+    "when provided with the number 7 return 3" in {
+      Count.countOdds(7) mustEqual 3
+    }
+    "when provided with the number 8 return 4 " in {
+      Count.countOdds(8) mustEqual 4
+    }
+
 
   }
 
